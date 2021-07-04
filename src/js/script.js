4189171f76f3020eab1b8f1efb83a4a09445014e
Anastasiia <nsydorchenko@gmail.com>
2021-07-04 18:41:11 +0300

    add intro, notice sections

---
+++ src/js/script.js
@@ -0,0 +1,8 @@
+//100VH MOBILE FIX
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+window.addEventListener('resize', () => {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
