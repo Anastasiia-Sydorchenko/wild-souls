3f1ab0d0b8a1f2f16a412f363aba07d8182c8f30
Anastasiia <nsydorchenko@gmail.com>
2021-07-05 14:53:53 +0300

    add slider and new sections

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,37 @@
+// Import jQuery module (npm i jquery)
+import $ from 'jquery'
+
+// import Swiper JS
+import Swiper from 'swiper';
+
+// core version + navigation, pagination modules:
+import SwiperCore, { Navigation, Pagination } from 'swiper/core';
+
+// configure Swiper to use modules
+SwiperCore.use([Navigation, Pagination]);
+
+//PRODUCTS SLIDER
+const productsSlider = new Swiper('#products-slider', {
+  loop: true,
+  slidesPerView: 3.5,
+  spaceBetween: 60,
+  lazyLoading: true,
+  speed: 1000,
+});
+
+//PROCESS SLIDER
+const processSlider = new Swiper('#process-slider', {
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 240,
+  lazyLoading: true,
+  speed: 1000,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
+
 //100VH MOBILE FIX
 let vh = window.innerHeight * 0.01;
 document.documentElement.style.setProperty('--vh', `${vh}px`);
